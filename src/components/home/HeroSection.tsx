ff2189e08b85a32486bff31da510927e1bdba808
omarfarukee <umarfaruque1920@gmail.com>
2025-02-28 21:46:55 +0600

    updated

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -8,21 +8,21 @@ const slides = [
   {
     id: 1,
     image: "/assets/machine/44.png",
-    title: "Demo Title",
+    title: "Demo Title lorem ipsum",
     video: "/assets/videos/slider1.mp4",
     subtitle: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   },
   {
     id: 2,
     image: "/assets/machine/Neww-03.png",
-    title: "Demo Title",
+    title: "Demo Title lorem ipsum",
     video: "/assets/videos/slider2.mp4",
     subtitle: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   },
   {
     id: 3,
     image: "/assets/machine/333.png",
-    title: "Demo Title",
+    title: "Demo Title lorem ipsum",
     video: "/assets/videos/slider3.mp4",
     subtitle: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   }
@@ -101,7 +101,7 @@ export default function HeroSection() {
                 ease: easeInOut
               }}
             >
-              <h2 className="text-3xl sm:text-4xl  lg:text-9xl text-[#fcd462] font-bold mb-2 sm:mb-4">
+              <h2 className="text-3xl sm:text-4xl  lg:text-6xl text-[#fcd462] font-bold mb-2 sm:mb-4">
                 {slides[currentSlide].title}
               </h2>
               {/* <h2 className="text-3xl absolute sm:text-4xl md:text-5xl lg:text-[20em] top-[-130%]  font-serif text-[#ffffff09] font-bold mb-2 sm:mb-4">
